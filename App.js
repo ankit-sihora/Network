7cfdff821990c953b25b749a2c721a25fc4bdaf4
ankit-sihora <ankit.sihora@bacancytechnology.com>
2019-02-18 12:48:28 +0530

    done with get image

--- App.js
+++ App.js
@@ -1,36 +1,40 @@
 import React, { Component } from 'react';
-import { TouchableOpacity, View, ActivityIndicator, Text, Alert,Image} from 'react-native';
+import { TouchableOpacity, View, ActivityIndicator, Text, Alert,Image,ScrollView} from 'react-native';
 export default class App extends Component {
   constructor(){
     super()
       this.state={
-        url:'',
+        source:'',
+        flag:true,
+        num:100,
       }
     this.handlePress.bind(this)
   }
-  
+
 handlePress = async () => {
-  fetch('https://api.github.com/users/komalvekariya/repos',{method: 'GET'}
+  fetch('https://api.github.com/users/ankit-sihora/repos',{method: 'GET'}
       
   )
   .then(response => response.json()).then((responseJson)=>{
     //alert('Response'+JSON.stringify(responseJson[0].owner.avatar_url))
-    this.setState({url:JSON.stringify(responseJson[0].owner.avatar_url)})
-    alert(this.state.url)
-  })  
+    this.setState({source:responseJson[0].owner.avatar_url,flag:false,num:50})
+    alert(this.state.source)
+  })
   .catch((error) => {
       console.error(error);
   });
 }
   render(){
+   
   return(
    <View style={{flex:1,paddingTop: 50, paddingLeft: 50, backgroundColor:'white' }}>
    <Text> Some other text </Text>
     <Text> Some other text </Text>
     <TouchableOpacity onPress={this.handlePress}>
      <Text style={{paddingTop: 50, paddingLeft: 50, color: '#FF0000'}}> Click me to see the name </Text>
-     </TouchableOpacity><TouchableOpacity onPress={()=>{this.forceUpdate(alert('hello')),Alert.alert(this.state.url)}}>
-    <Image style={{height:100,width:100}} source={{url:this.state.url}}></Image></TouchableOpacity>
+     </TouchableOpacity><ScrollView><TouchableOpacity style={{backgroundColor:'green',margin:30}} onPress={()=>{Alert.alert(this.state.source)}}>
+     {(!this.state.flag)?
+    <Image style={{height:100,width:100}} source={{url:this.state.source}}></Image>:(<View></View>)}</TouchableOpacity></ScrollView>
 </View> 
   );
 }
